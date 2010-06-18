84868bc25a2dddc4f07abf3cedf16c44f52fc327
Maxim Filatov <pipopolam@gmail.com>
2010-06-18 16:48:48 +0400

    Admin is not registerable

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,7 +2,7 @@ class Admin < ActiveRecord::Base
 
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable, :lockable, :timeoutable
 
   # Setup accessible (or protected) attributes for your model
